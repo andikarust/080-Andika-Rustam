65dcdb6e06b4e063a74ead17888175c5af1ee655
Andika Rustam <andikarustam1203@gmail.com>
2024-05-15 23:39:29 +0700

    Membuat view dan routing halaman admin organisasi

--- AplikasiVolunteer/routes/web.php
+++ AplikasiVolunteer/routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
+
+Route::get('/admin/profile', function () {
+    return view('admin.profile');
+})->name('admin.profile');
+
+Route::get('/admin/acara', function () {
+    return view('admin.acara');
+})->name('admin.acara');
+
+Route::get('/admin/tambah', function () {
+    return view('admin.tambah');
+})->name('admin.tambah');
